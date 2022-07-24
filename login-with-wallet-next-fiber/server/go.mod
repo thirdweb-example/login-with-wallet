a533e7d19550e9e8dda20c0303d73861107e522d
adam-maj <mr.adam.maj@gmail.com>
2022-07-24 16:08:16 -0700

    Update to use new Go and TS SDK

--- login-with-wallet-next-fiber/server/go.mod
+++ login-with-wallet-next-fiber/server/go.mod
@@ -20,7 +20,7 @@ require (
 	github.com/rjeczalik/notify v0.9.2 // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
-	github.com/thirdweb-dev/go-sdk v1.2.6 // indirect
+	github.com/thirdweb-dev/go-sdk v1.2.8 // indirect
 	github.com/tklauser/go-sysconf v0.3.10 // indirect
 	github.com/tklauser/numcpus v0.5.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- login-with-wallet-next-fiber/server/main.go
+++ login-with-wallet-next-fiber/server/main.go
@@ -1,46 +1,16 @@
 package main
 
 import (
-	"encoding/hex"
-	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"github.com/thirdweb-dev/go-sdk/thirdweb"
 )
 
-type WalletLoginPayload struct {
-	Payload struct {
-		Payload struct {
-			Domain         string    `json:"domain"`
-			Address        string    `json:"address"`
-			Nonce          string    `json:"nonce"`
-			ExpirationTime time.Time `json:"expirationTime"`
-			ChainId        int       `json:"chainId"`
-		}
-		Signature string `json:"signature"`
-	} `json:"payload"`
-}
-
-func (w *WalletLoginPayload) ToThirdWeb() (*thirdweb.WalletLoginPayload, error) {
-	signature, err := hex.DecodeString(w.Payload.Signature[2:])
-	if err != nil {
-		return nil, err
-	}
-
-	return &thirdweb.WalletLoginPayload{
-		Payload: &thirdweb.WalletLoginPayloadData{
-			Domain:         w.Payload.Payload.Domain,
-			Address:        w.Payload.Payload.Address,
-			Nonce:          w.Payload.Payload.Nonce,
-			ExpirationTime: w.Payload.Payload.ExpirationTime,
-			ChainId:        w.Payload.Payload.ChainId,
-		},
-		Signature: signature,
-	}, nil
+type LoginPayload struct {
+	Payload *thirdweb.WalletLoginPayload `json:"payload"`
 }
 
 func main() {
@@ -52,14 +22,9 @@ func main() {
 	app := fiber.New()
 
 	app.Post("/login", func(c *fiber.Ctx) error {
-		w := new(WalletLoginPayload)
+		w := new(LoginPayload)
 
 		if err := c.BodyParser(w); err != nil {
-			return err
-		}
-
-		payload, err := w.ToThirdWeb()
-		if err != nil {
 			return err
 		}
 
@@ -70,14 +35,12 @@ func main() {
 			return err
 		}
 
+		payload := w.Payload
 		domain := "thirdweb.com"
 		token, err := sdk.Auth.GenerateAuthToken(domain, payload, nil)
 		if err != nil {
-			fmt.Println(err)
 			return err
 		}
-
-		fmt.Println(token)
 
 		return c.Status(200).JSON(&fiber.Map{
 			"address": token,
