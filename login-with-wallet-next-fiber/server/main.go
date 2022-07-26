506c1482a10ff7b6eb0977ffb9fa75b60de9ab4c
adam-maj <mr.adam.maj@gmail.com>
2022-07-26 14:13:27 -0700

    Add completed login-with-wallet-next-fiber example

--- login-with-wallet-next-fiber/server/main.go
+++ login-with-wallet-next-fiber/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"os"
 
@@ -28,23 +30,73 @@ func main() {
 			return err
 		}
 
+		privateKey := os.Getenv("ADMIN_PRIVATE_KEY")
+		if privateKey == "" {
+			fmt.Printf("Missing ADMIN_PRIVATE_KEY environment variable")
+			return errors.New("Admin private key not set")
+		}
+
 		sdk, err := thirdweb.NewThirdwebSDK("mumbai", &thirdweb.SDKOptions{
-			PrivateKey: os.Getenv("ADMIN_PRIVATE_KEY"),
+			PrivateKey: privateKey,
 		})
 		if err != nil {
 			return err
 		}
 
+		// Get signed login payload from the frontend
 		payload := w.Payload
+
+		// Generate an access token with the SDK using the signed payload
 		domain := "thirdweb.com"
 		token, err := sdk.Auth.GenerateAuthToken(domain, payload, nil)
 		if err != nil {
 			return err
 		}
 
-		return c.Status(200).JSON(&fiber.Map{
-			"address": token,
+		// Securely set httpOnly cookie on request to prevent XSS on frontend
+		// And set path to / to enable access_token usage on all endpoints
+		c.Cookie(&fiber.Cookie{
+			Name:     "access_token",
+			Value:    token,
+			Path:     "/",
+			HTTPOnly: true,
+			Secure:   true,
+			SameSite: "strict",
 		})
+
+		return c.SendStatus(200)
+	})
+
+	app.Post("/authenticate", func(c *fiber.Ctx) error {
+		privateKey := os.Getenv("ADMIN_PRIVATE_KEY")
+		if privateKey == "" {
+			fmt.Printf("Missing ADMIN_PRIVATE_KEY environment variable")
+			return errors.New("Admin private key not set")
+		}
+
+		sdk, err := thirdweb.NewThirdwebSDK("mumbai", &thirdweb.SDKOptions{
+			PrivateKey: privateKey,
+		})
+		if err != nil {
+			return err
+		}
+
+		// Get access token off cookies
+		token := c.Cookies("access_token")
+		if token == "" {
+			return c.SendStatus(401)
+		}
+
+		// Authenticate token with the SDK
+		domain := "thirdweb.com"
+		address, err := sdk.Auth.Authenticate(domain, token)
+
+		return c.Status(200).JSON(address)
+	})
+
+	app.Post("/logout", func(c *fiber.Ctx) error {
+		c.ClearCookie("access_token")
+		return c.SendStatus(200)
 	})
 
 	log.Fatal(app.Listen(":8000"))
